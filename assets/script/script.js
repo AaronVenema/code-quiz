b0aa103eefbd253e66e976679177fb3771815141
AaronVenema <34764586+AaronVenema@users.noreply.github.com>
2022-09-22 00:41:48 -0500

    complete revisions of css, js, and html after four hours and youtube

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,123 +1,205 @@
-const question = document.getElementById('question');
-const a = document.getElementById('a+');
-const b = document.getElementById('b+');
-const c = document.getElementById('c+');
-const d = document.getElementById('d+');
-var btn = document.getElementById('submit');
-const all_answer = document.querySelectorAll('.answers');
-const Timer_Text= document.getElementsByClassName(`timer-text`)[0];
-
-
+var resultsEl = document.getElementById("result");
+var questionsEl = document.getElementById("questions");
+var btnA = document.getElementById("a");
+var btnB = document.getElementById("b");
+var btnC = document.getElementById("c");
+var btnD = document.getElementById("d");
+var startButton = document.getElementById("startBtn");
+var startQuizDiv = document.getElementById("startPg");
+var quizTimer = document.getElementById("timer");
+var endGameBtns = document.getElementById("endGameBtns");
+var submitScoreBtn = document.getElementById("submitScore");
+var highscoreDisplayScore = document.getElementById("hsS")
+var hsCont = document.getElementById("hsCont");
+var hsPg = document.getElementById("hsPg");
+var highscoreInputName = document.getElementById("initials");
+var hsInit = document.getElementById("hsInit");
+var endBtns = document.getElementById("endBtns");
+var submitScoreBtn = document.getElementById("submitScore");
+var hsS = document.getElementById("hsS");
+var fSEl = document.getElementById("fS");
+var gameoverDiv = document.getElementById("gameover");
+var quizBody = document.getElementById("quiz");
 /*quiz information */
-const quizData = [
+var quizData = [
     {
-      question: 'Read directions, when ready to start select go and click submit',
-      a: 'bored',
-      b: 'go',
-      c: 'no',
-      d: `yes`,
-      correct: 'b',
+      question: "Who's Garys favorite student?",
+      a: "Joe",
+      b: "Julia",
+      c: "Aaron",
+      d: "Tyler",
+      correctAnswer: "b",
     },  
   {
-      question: 'How many kids does Aaron have ?',
-      a: '1',
-      b: '2',
-      c: '3',
-      d: `4`,
-      correct: 'b',
+      question: "How many kids does Aaron have ?",
+      a: "1",
+      b: "2",
+      c: "3",
+      d: "4",
+      correctAnswer: "b",
     },
     {
-      question: 'How old is Gary ?',
-      a: '25',
-      b: '35',
-      c: '56',
-      d: `to old`,
-      correct: 'c',
+      question: "How old is Gary ?",
+      a: "25",
+      b: "35",
+      c: "56",
+      d: "to old",
+      correctAnswer: "c",
     },
     {
-      question: 'What color is the Sea ?',
-      a: 'Green ',
-      b: 'Blue ',
-      c: 'Red ',
-      d: `Yellow `,
-      correct: 'b',
+      question: "What color is the Sea ?",
+      a: "Green",
+      b: "Blue",
+      c: "Red",
+      d: "Yellow",
+      correctAnswer: "b",
     },
     {
-      question: 'What is Garys degree in?',
-      a: 'English ',
-      b: 'Under water basket weaving ',
-      c: 'Coding ',
-      d: 'He doesnt have one',
-      correct: 'a',
+      question: "What is Garys degree in?",
+      a: "English",
+      b: "Under water basket weaving",
+      c: "Coding",
+      d: "He doesnt have one",
+      correctAnswer: "a",
+    },
+    {
+      question: "What is Garys alcohol of choice?",
+      a: "Malt liquor",
+      b: "Coors",
+      c: "All of them",
+      d: "Tequila",
+      correctAnswer: "d",
+    },
+    {
+      question: "Whos going to pass this bootcamp?",
+      a: "No one",
+      b: "Not Julia",
+      c: "Everybody",
+      d: "Just Joe",
+      correctAnswer: "c",
     },
   ];
+  var finalQuestionIndex = quizData.length;
+  var currentQuestionIndex = 0;
+  var score = 0;
+  var correct;
+  var timerInterval;
+  var timeLeft= 60;
 
-let index = 0;
-let score = 0;
+function generateQuestion(){
+  gameoverDiv.style.display = "none";
+  if(currentQuestionIndex === finalQuestionIndex){
+    return showScore();
+  } 
+  var currentQuestion = quizData[currentQuestionIndex];
+      questionsEl.innerHTML = "<p>" + currentQuestion.question + "</p>";
+      btnA.innerHTML = currentQuestion.a;
+      btnB.innerHTML = currentQuestion.b;
+      btnC.innerHTML = currentQuestion.c;
+      btnD.innerHTML = currentQuestion.d;
+};
 
-function selectans() {
-  let ans = undefined;
-  all_answer.forEach((el) => {
-    if (el.checked) {
-      ans = el.id;
-    }
-  });
-  return ans;
+function startQuiz(){
+  startQuizDiv.style.display = "none";
+  gameoverDiv.style.display = "none";
+  generateQuestion();
+
+timerInterval = setInterval(function() {
+  timeLeft--;
+  quizTimer.textContent = "Time left: " + timeLeft;
+  if(timeLeft === 0) {
+    clearInterval(timerInterval);
+    showScore();
+  }
+}, 1000);
+quizBody.style.display = "flex";
 }
 
-function unselectans() {
-  all_answer.forEach((el) => {
-    el.checked = false;
-  });
+function showScore(){
+  quizBody.style.display = "none"
+  gameoverDiv.style.display = "flex";
+  clearInterval(timerInterval);
+  highscoreInputName.value = "";
+  fSEl.innerHTML = "You got " + score + " out of " + quizData.length + " correct!";
 }
 
-function getquiz() {
-  unselectans();
-  question.innerText = quizData[index].question;
-  a.innerText = quizData[index].a;
-  b.innerText = quizData[index].b;
-  c.innerText = quizData[index].c;
-  d.innerText = quizData[index].d;
-}
-
-var timer= null;
-var timerCount= 60;
-function startTimer(){
-  timer= setInterval(function() {
-    timerCount--;
-    if(timerCount === 0) { 
-      clearInterval(timer);
-    }
-    Timer_Text.innerHTML=timerCount;
-  }, 1000);
-}
-console.log(timerCount)
-btn.addEventListener("click", function(event) {
-  startTimer();
+submitScoreBtn.addEventListener("click", function highscore(){    
+  if(highscoreInputName.value === "") {
+      alert("Must provide name");
+      return false;
+  }else{
+      var savedHighscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
+      var currentUser = highscoreInputName.value.trim();
+      var currentHighscore = {
+          name : currentUser,
+          score : score
+      };
+      gameoverDiv.style.display = "none";
+      hsCont.style.display = "flex";
+      hsPg.style.display = "flex";
+      endBtns.style.display = "flex";
+      savedHighscores.push(currentHighscore);
+      localStorage.setItem("savedHighscores", JSON.stringify(savedHighscores));
+      generateHighscores();
+  } 
 });
 
-console.log(timer)
-
-function startquiz() {
-  btn.addEventListener('click', () => {
-    let ans = selectans();
-    if (ans) {
-      if (ans == quizData[index].correct) {
-        score++;
-      }
-    }
-    index++;
-    if (index < quizData.length) {
-      getquiz();
-    } else {
-      alert('score :' + score);
-      location.reload();
-    }
-  });
+function generateHighscores(){
+  hsInit.innerHTML = "";
+  hsS.innerHTML = "";
+  var highscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
+  for (i=0; i<highscores.length; i++){
+      var newNameSpan = document.createElement("li");
+      var newScoreSpan = document.createElement("li");
+      newNameSpan.textContent = highscores[i].name;
+      newScoreSpan.textContent = highscores[i].score;
+      hsInit.appendChild(newNameSpan);
+      hsS.appendChild(newScoreSpan);
+  }
 }
-getquiz();
-startquiz();
+function showHighscore(){
+  startQuizDiv.style.display = "none"
+  gameoverDiv.style.display = "none";
+  hsCont.style.display = "flex";
+  hsPg.style.display = "flex";
+  endBtns.style.display = "flex";
 
+  generateHighscores();
+}
+// Clears scores and local storage
+function clearScore(){
+  window.localStorage.clear();
+  hsInit.textContent = "";
+  hsS.textContent = "";
+}
 
+// replay this quiz
+function replayQuiz(){
+  hsCont.style.display = "none";
+  gameoverDiv.style.display = "none";
+  startQuizDiv.style.display = "flex";
+  timeLeft = 60;
+  score = 0;
+  currentQuestionIndex = 0;
+}
+// Function checks answer in HTML
+function checkAnswer(answer){
+  correct = quizData[currentQuestionIndex].correctAnswer;
 
-
+  if (answer === correct && currentQuestionIndex !== finalQuestionIndex){
+      score++;
+      alert("That Is Correct!");
+      currentQuestionIndex++;
+      generateQuestion();
+      //displays the answer is correct.
+  }else if (answer !== correct && currentQuestionIndex !== finalQuestionIndex){
+      alert("That Is Incorrect.")
+      currentQuestionIndex++;
+      timeLeft-=5;
+      generateQuestion();
+      //displays the answer is wrong.
+  }else{
+      showScore();
+  }
+}
+startButton.addEventListener("click",startQuiz);
